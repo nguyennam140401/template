08e0e1b86cf8b1cd9f1a90c32173389d4a060434
nam140401 <nguyennam140401@gmail.com>
2021-10-20 00:26:53 +0700

    update 20-10

--- 20-10/data.js
+++ 20-10/data.js
@@ -1,58 +1,77 @@
 const data = [
     {
-        name: 'ABCD',
-        message: 'bla bla bl',
+        name: 'Kim Phượng',
+        message:
+            'Là cô gái khá cá tính nhưng luôn luôn nhiệt tình,thân thiện với bạn bè. ',
         img: './img/1.jpg',
     },
     {
-        name: 'ABCD',
-        message: 'bla bla bl',
+        name: 'Trà My',
+        message:
+            'Cô gái có nụ cười tươi, luôn luôn vui vẻ, truyền năng lượng tích cực đến mọi người.',
         img: './img/2.jpg',
     },
     {
-        name: 'ABCD',
-        message: 'bla bla bl',
+        name: 'Trương Phương',
+        message:
+            'Lớp phó đời sống luôn luôn quan tâm đến mọi người dù đôi khi có hơi " nóng tính như kem" nhưng cũng là vì các bạn',
         img: './img/3.jpg',
     },
     {
-        name: 'ABCD',
-        message: 'bla bla bl',
+        name: 'Ngọc Hải',
+        message:
+            'Lớp phó học tập xinh đẹp nên môn nào cũng A , luôn nhắc nhở ,giúp đỡ các bạn chăm chỉ trong học tập ',
         img: './img/4.jpg',
     },
     {
-        name: 'ABCD',
-        message: 'bla bla bl',
+        name: 'Thùy Linh',
+        message:
+            ' Lớp trưởng xinh gái , đáng yêu ,người luôn có trách nhiệm cao trong công việc , luôn quan tâm và giúp đỡ chúng tớ trong các hoạt động nè. ',
         img: './img/5.jpg',
     },
     {
-        name: 'ABCD',
-        message: 'bla bla bl',
+        name: 'Hoài Linh',
+        message:
+            'thủ quỹ đến từ Hà Tĩnh , cô gái xinh xắn đáng yêu luôn yêu đời và luôn được mọi người yêu quý',
         img: './img/6.jpg',
     },
     {
         name: 'Bé Nguyên',
-        message: 'Lời chúc ',
+        message:
+            'cô vợ quốc dân xinh đẹp, thân thiện với mọi người, luôn vui vẻ và tràn đầy năng lượng',
         img: './img/7.jpg',
     },
     {
-        name: 'ABCD',
-        message: 'bla bla bl',
+        name: 'Tú Anh',
+        message:
+            ' Là mẫu người đại diện cho câu nói con nhà người ta khi đạt điểm cao với hầu hết tất cả các môn và cũng rất hay nhắc bài cho các bạn nữa .',
         img: './img/8.jpg',
     },
     {
-        name: 'ABCD',
-        message: 'bla bla bl',
+        name: 'Kim',
+        message: 'Cô gái bí ẩn của lớp, trầm tính nhưng học thì siêu đỉnh,.',
         img: './img/9.jpg',
     },
     {
-        name: 'ABCD',
-        message: 'bla bla bl',
-        img: './img/10.jpg',
+        name: 'Thùy Linh',
+        message: 'bla bla',
+        img: './img/5.jpg',
     },
 ]
-
+const audioElement = document.querySelector('audio')
+// window.addEventListener('load', () => {
+//     console.log(1)
+//     setTimeout(() => {
+//         audioElement.play()
+//     }, 1000)
+// })
 const ring = document.querySelector('.ring')
-const result = data.map((item) => {
+const result = data.map((item, idx) => {
+    if (idx == 9) {
+        return `<div class="img">
+        
+    </div>`
+    }
     return `
     <div class="img">
         <div class="content">
@@ -63,6 +82,7 @@ const result = data.map((item) => {
     </div>
     `
 })
+
 const res = result.join('')
 ring.innerHTML = res
 const listImg = document.querySelectorAll('.img')
